857b6cba3bd9f1d4fe7c2f3df006987a1ede6367
frndxyz <pravin_bhudiya@yahoo.com>
2020-08-27 10:08:01 +0530

    add testnet run support with name contain testnet on linux.

--- contrib/build-linux/appimage/apprun.sh
+++ contrib/build-linux/appimage/apprun.sh
@@ -3,9 +3,16 @@
 set -e
 
 APPDIR="$(dirname "$(readlink -e "$0")")"
+APPNAME="$(basename "$ARGV0")"
 
 export LD_LIBRARY_PATH="${APPDIR}/usr/lib/:${APPDIR}/usr/lib/x86_64-linux-gnu${LD_LIBRARY_PATH+:$LD_LIBRARY_PATH}"
 export PATH="${APPDIR}/usr/bin:${PATH}"
 export LDFLAGS="-L${APPDIR}/usr/lib/x86_64-linux-gnu -L${APPDIR}/usr/lib"
 
-exec "${APPDIR}/usr/bin/python3.7" -s "${APPDIR}/usr/bin/electrum" "$@"
+if [[ $APPNAME == *"testnet"* ]];
+then
+  exec "${APPDIR}/usr/bin/python3.7" -s "${APPDIR}/usr/bin/electrum" "--testnet"
+else
+  exec "${APPDIR}/usr/bin/python3.7" -s "${APPDIR}/usr/bin/electrum" "$@"
+fi
+
